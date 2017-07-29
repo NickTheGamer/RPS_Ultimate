d1001137ad5f5a475c8f1d7182d8c6b6a34d7bbc
NickTheGamer <nic.coetzee2014@gmail.com>
2017-07-29 13:44:40 +1200

    RPS Ultimate Edition
    Our first attempt at pushing to Github

---
+++ app/src/main/java/com/example/coetzeem/rps_ultimate/challenge_send.java
@@ -0,0 +1,13 @@
+package com.example.coetzeem.rps_ultimate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class challenge_send extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_send);
+    }
+}
---
+++ app/src/main/java/com/example/coetzeem/rps_ultimate/join_league.java
@@ -0,0 +1,13 @@
+package com.example.coetzeem.rps_ultimate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class join_league extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_league);
+    }
+}
