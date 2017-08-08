475d493c57e513fd5df5b4d8ead692fb5fba40a2
NickTheGamer <nic.coetzee2014@gmail.com>
2017-08-08 19:53:36 +1200

    renamed challenge_screen to recievedchallenge_table, edited and edited sent challenge table

--- app/src/main/java/com/example/coetzeem/rps_ultimate/recievechallenge_table.java
+++ app/src/main/java/com/example/coetzeem/rps_ultimate/recievechallenge_table.java
@@ -3,11 +3,11 @@ package com.example.coetzeem.rps_ultimate;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class recieve_challenge extends AppCompatActivity {
+public class recievechallenge_table extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.challenge_screen);
+        setContentView(R.layout.recievechallenge_table);
     }
 }
