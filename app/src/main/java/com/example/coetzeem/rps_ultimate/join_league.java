2f4c23bc1e451f0877513125edfd65639f0c2a10
infernohound33 <ml-110884@remint.school.nz>
2017-08-17 17:58:32 +1200

    more activity transitons

--- app/src/main/java/com/example/coetzeem/rps_ultimate/join_league.java
+++ app/src/main/java/com/example/coetzeem/rps_ultimate/join_league.java
@@ -1,13 +1,26 @@
 package com.example.coetzeem.rps_ultimate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class join_league extends AppCompatActivity {
+
+    public void onButtonClick(View v)
+    {
+        if(v.getId() == R.id.BC  )
+        {
+            Intent i = new Intent(join_league.this, Every_other_time.class);
+            startActivity(i);
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join_league);
+        setContentView(R.layout.activity_first_screen);
     }
 }
